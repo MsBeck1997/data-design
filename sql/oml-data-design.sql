d130309b9036bdc47b5f3653516dfc60d850b19b
Sarah Beck <sarah.lizzie.beck@gmail.com>
2020-05-07 10:57:47 -0600

    added oml page and documentation for it

---
+++ sql/oml-data-design.sql
@@ -0,0 +1,10 @@
+-- OML
+-- Write and execute three insert statements for an autotrophic table in your database
+-- Write and execute write an update statement on one of the rows created in the previous step,
+-- Write and execute a delete statement on another row created from your insert statements.
+-- Write and execute an insert statement on a table with a foreign key from the original table.
+-- Write and execute a select statement for a row using its primary key as the selector.
+-- Write and execute a select statement that incorporates both a where clause and an inner-join on both tables
+    -- used in this assignment.
+-- Write a select statement based of off DDC-Twitter that counts the number of likes for a specific tweet.
+
