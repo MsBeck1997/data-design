87a0306aaf2c457f1e16bcd94071b98564094b67
Sarah Beck <sarah.lizzie.beck@gmail.com>
2020-05-12 13:37:15 -0600

    fixing ID's

--- sql/oml-data-design.sql
+++ sql/oml-data-design.sql
@@ -2,26 +2,27 @@
 
 
 -- Write and execute three insert statements for an autotrophic table in your database
-INSERT INTO profile VALUES (HEX('1'), null, 'ariljohnson@gmail.com', 'abc123', 'ArinJ', null, HEX(123456789)),
-                           (HEX('2'), null, 'feliciakeys@gmail.com', 'dogsRcool', 'PuppyFelicia', null, HEX(223456789)),
-                           (HEX('3'), null, 'warren14532@yahoo.com', 'bananas!', 'BananaRama', null, HEX(323456789));
+INSERT INTO profile VALUES (UNHEX('1ed673e1153044199b6cf38f920dacf0'), null, 'arinjohnson@gmail.com', 'abc123', 'ArinJ', null, HEX(123456789)),
+                           (UNHEX('2ed673e1153044199b6cf38f920dacf0'), null, 'feliciakeys@gmail.com', 'dogsRcool', 'PuppyFelicia', null, HEX(223456789)),
+                           (UNHEX('3ed673e1153044199b6cf38f920dacf0'), null, 'warren14532@yahoo.com', 'bananas!', 'BananaRama', null, HEX(323456789));
 
 -- Write and execute write an update statement on one of the rows created in the previous step,
-UPDATE profile SET profileDisplayName = 'ArinDaBoss' WHERE profileEmail = 'ariljohnson@gmail.com';
+UPDATE profile SET profileDisplayName = 'ArinDaBoss' WHERE profileId = HEX('1ed673e1153044199b6cf38f920dacf0');
 
 -- Write and execute a delete statement on another row created from your insert statements.
-DELETE FROM profile WHERE profileEmail = 'feliciakeys@gmail.com';
+DELETE FROM profile WHERE profileId = HEX('2ed673e1153044199b6cf38f920dacf0');
 
 -- Write and execute an insert statement on a table with a foreign key from the original table.
-INSERT INTO gameInformation VALUES (HEX('a'), (SELECT profileId FROM profile WHERE profileEmail='ariljohnson@gmail.com'),
-                                    'Minecraft', NULL, 'Live');
+INSERT INTO gameInformation VALUES (UNHEX('aed673e1153044199b6cf38f920dacf0'), (SELECT profileId FROM profile
+                                    WHERE (profileId = HEX('1ed673e1153044199b6cf38f920dacf0'))),'Minecraft',
+                                    NULL, 'Live');
 
 -- Write and execute a select statement for a row using its primary key as the selector. ERROR
-SELECT * FROM profile WHERE profileId = '31';
+SELECT * FROM profile WHERE profileId = HEX('1ed673e1153044199b6cf38f920dacf0');
 
 -- Write and execute a select statement that incorporates both a where clause and an inner-join on both tables
 --      used in this assignment.
-SELECT * FROM profile WHERE profileId = '31' INNER JOIN profile.profileId = gameInformation.gameHost;
+SELECT * FROM profile WHERE profileId = HEX('1ed673e1153044199b6cf38f920dacf0') INNER JOIN profile.profileId = gameInformation.gameHost;
 
 -- Write a select statement based of off DDC-Twitter that counts the number of likes for a specific tweet.
 --      Using information not currently in SQL tables, this is realistic fiction - DO NOT RUN
