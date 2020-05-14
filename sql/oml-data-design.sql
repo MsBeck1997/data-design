fb97ea961df4ad19412f807df56819c5e848403e
Sarah Beck <sarah.lizzie.beck@gmail.com>
2020-05-14 08:51:30 -0600

    corrected all errors

--- sql/oml-data-design.sql
+++ sql/oml-data-design.sql
@@ -14,15 +14,15 @@ DELETE FROM profile WHERE profileId = HEX('2ed673e1153044199b6cf38f920dacf0');
 
 -- Write and execute an insert statement on a table with a foreign key from the original table.
 INSERT INTO gameInformation VALUES (UNHEX('aed673e1153044199b6cf38f920dacf0'), (SELECT profileId FROM profile
-                                    WHERE (profileId = HEX('1ed673e1153044199b6cf38f920dacf0'))),'Minecraft',
+                                    WHERE profileId = UNHEX('1ed673e1153044199b6cf38f920dacf0')),'Minecraft',
                                     NULL, 'Live');
 
--- Write and execute a select statement for a row using its primary key as the selector. ERROR
+-- Write and execute a select statement for a row using its primary key as the selector.
 SELECT HEX(profileId),profileDisplayName FROM profile WHERE profileId = UNHEX('1ed673e1153044199b6cf38f920dacf0');
 
 -- Write and execute a select statement that incorporates both a where clause and an inner-join on both tables
 --      used in this assignment.
-SELECT profileDisplayName FROM profile INNER JOIN gameInformation ON profile.profileId = gameInformation.gameHost;
+SELECT profileDisplayName,gameTitle FROM profile INNER JOIN gameInformation ON profile.profileId = gameInformation.gameHost;
 
 -- Write a select statement based of off DDC-Twitter that counts the number of likes for a specific tweet.
 --      Using information not currently in SQL tables, this is realistic fiction - DO NOT RUN
